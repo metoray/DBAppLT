813d9bd36a2fad875579cf9504c9cdd2f62979c8
metoray <metoray@gmail.com>
2015-09-23 20:00:05 +0200

    Changed data structure to EnumMap

--- src/unwdmi/dbapp/Measurement.java
+++ src/unwdmi/dbapp/Measurement.java
@@ -1,17 +1,30 @@
 package unwdmi.dbapp;
 
 import java.util.Date;
+import java.util.EnumMap;
 
 /**
  * Created by metoray on 23-9-15.
  */
 public class Measurement {
 
-    public final static String[] types = new String[]{"TEMP","DEWP","STP","SLP","VISIB","WDSP","PRCP","SNDP","CLDC","WNDDIR"};
-
-    private Number[] data;
     private Date date;
     private int stationID;
+
+    public static enum Type{
+        TEMP,
+        DEWP,
+        STP,
+        SLP,
+        VISIB,
+        WDSP,
+        PRCP,
+        SNDP,
+        CLDC,
+        WINDDIR
+    }
+
+    private EnumMap<Type,Number> data;
 
     /**
      * Constructor for measurement
@@ -19,13 +32,9 @@ public class Measurement {
      * @param date the date and time the measurement was made
      * @param data an array containing all measurements in the order described in types.
      */
-    public Measurement(int stationID, Date date, Number[] data){
-        if(data.length!=types.length){
-            throw new IllegalArgumentException("Data array does not contain correct amount of data.");
-        }
+    public Measurement(int stationID, Date date, EnumMap<Type,Number> data){
         this.data = data;
         this.date = date;
         this.stationID = stationID;
     }
-
 }
