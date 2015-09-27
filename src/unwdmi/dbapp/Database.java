0168d36e9f2f988b003357e3c845e3a8403248bc
mrdizzer <j.koppelaar@st.hanze.nl>
2015-09-27 22:59:20 +0200

    Insert Finished

--- src/unwdmi/dbapp/Database.java
+++ src/unwdmi/dbapp/Database.java
@@ -1,38 +1,55 @@
 package unwdmi.dbapp;
 import java.sql.*;
+import java.util.EnumMap;
+import java.util.LinkedList;
 
 public class Database {
-
-	public static void main(String[] args){
-		insertMeasurement(1,2,3,4,5,6,7,8,9,10);
-	}
 	
-	public static void insertMeasurement(Measurement measurement) {
-
+	private Connection conn;
+	
+	public Database(){
 		try{
 			String myDriver = "com.mysql.jdbc.Driver";
-			String myUrl = "jdbc:mysql://localhost/test";
+			String myUrl = "jdbc:mysql://localhost/unwdmi";
 			Class.forName(myDriver);
-			Connection conn = DriverManager.getConnection(myUrl, "root", "pass");
-			
-			String query ="insert into measurement (TEMP, DEWP, STP, SLP, VISIB, WDSP, PRCP, SNDP,"
-					+ " CLDC, WINDIR) Values (?,?,?,?,?,?,?,?,?,?)";
-			PreparedStatement preparedStmt = conn.prepareStatement(query);
-   	      	
-			
-			preparedStmt.setDouble (1, TEMP);
-   	      	preparedStmt.setDouble (2, dewp);
-   	      	preparedStmt.setDouble (3, stp);
-   	     	preparedStmt.setDouble (4, slp);
-   	  		preparedStmt.setDouble (5, visib);
-   			preparedStmt.setDouble (6, wdsp);
-   			preparedStmt.setDouble (7, prcp);
-   			preparedStmt.setDouble (8, sndp);
-   			preparedStmt.setDouble (9, cldc);
-   			preparedStmt.setDouble (10, windir);
-   			
-   			System.out.println("shits done");
-	      }
+			this.conn = DriverManager.getConnection(myUrl, "root", "pass");
+		}catch(Exception ex)
+		{
+			System.out.println(ex);
+		}
+		
+	}
+	public void insertMeasurement(Measurement measurement) {
+
+	try{
+		MeasurementType[] types = MeasurementType.values();
+        String names = "(stn, date";
+        for(MeasurementType type: types){
+            names += ","+type.getColumnName();
+        }
+        names += ")";
+        //MAKE A QUERY USING THE NAMES VARIABLE HERE
+        String query = "INSERT INTO Measurements "+names+" VALUES(";
+        for(MeasurementType type: types){
+        	query +="?,";
+        }
+        query = query.substring(0, query.length()-1);
+        query += ")"; 
+        PreparedStatement preparedStmt = conn.prepareStatement(query);
+        preparedStmt.setInt(1,measurement.getStationID());
+        preparedStmt.setTimestamp(2, new java.sql.Timestamp(measurement.getDate().getTime()));
+        int i=2;
+        for(MeasurementType type: types){
+    	   i++;
+    	   Number value = measurement.getData(type);
+           if(value instanceof Integer){
+        	   preparedStmt.setInt(i,value.intValue());
+           }
+           else{
+        	   preparedStmt.setDouble(i,value.doubleValue());
+        	   }
+           }
+        }
 		catch(Exception ex){
 			System.out.println(ex);
 		}
@@ -41,15 +58,17 @@ public class Database {
 	public static void selectStatement(String colum){
 		
 		try{
+			//prepare connection
 			String myDriver = "com.mysql.jdbc.Driver";
 			String myUrl = "jdbc:mysql://localhost/test";
 			Class.forName(myDriver);
 			Connection conn = DriverManager.getConnection(myUrl, "root", "pass");
-			
+			//Select query
 			String query ="select "+colum+(" from measurment");
 			Statement st = conn.createStatement();
 			ResultSet rs = st.executeQuery(query);
 			
+			//loop trough resultlist
 			while(rs.next()){
 				
 				System.out.println(rs.getDouble(colum));
