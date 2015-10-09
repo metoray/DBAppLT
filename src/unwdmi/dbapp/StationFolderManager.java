eabdde81b1ae05fdee12576bd9da5d95a4d54753
metoray <metoray@gmail.com>
2015-10-09 17:05:13 +0200

    Added methods to get a filehandler from a measurement directly.

--- src/unwdmi/dbapp/StationFolderManager.java
+++ src/unwdmi/dbapp/StationFolderManager.java
@@ -2,6 +2,7 @@ package unwdmi.dbapp;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.concurrent.ConcurrentHashMap;
 
 //TODO: Replace 'Thread' with actual filehandler objects
@@ -14,6 +15,16 @@ public class StationFolderManager {
     public static String getMeasurementFolder(Measurement m) {
         SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
         return DATA_FOLDER + File.separator + m.getStationID() + File.separator + format.format(m.getDate()) + File.separator;
+    }
+
+    public static String getFileName(Measurement m){
+        Calendar c = Calendar.getInstance();
+        c.setTime(m.getDate());
+        return c.get(Calendar.HOUR_OF_DAY)+".csv";
+    }
+
+    public static Thread getFileHandler(Measurement m){
+        return getFileHandler(getMeasurementFolder(m)+getFileName(m));
     }
 
     public static Thread getFileHandler(String path) {
