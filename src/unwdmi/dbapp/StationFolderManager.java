7cc28337043e0d1fdf892763296bd989df22a9bb
metoray <metoray@gmail.com>
2015-10-09 16:57:55 +0200

    Started working on manager for filehandlers.

--- src/unwdmi/dbapp/StationFolderManager.java
+++ src/unwdmi/dbapp/StationFolderManager.java
@@ -2,17 +2,29 @@ package unwdmi.dbapp;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
+import java.util.concurrent.ConcurrentHashMap;
 
-/**
- * Created by metoray on 8-10-15.
- */
+//TODO: Replace 'Thread' with actual filehandler objects
 public class StationFolderManager {
+
+    private static ConcurrentHashMap<String,Thread> fileHandlers = new ConcurrentHashMap<>();
 
     public static String DATA_FOLDER = "data";
 
     public static String getMeasurementFolder(Measurement m) {
         SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
         return DATA_FOLDER + File.separator + m.getStationID() + File.separator + format.format(m.getDate()) + File.separator;
+    }
+
+    public static Thread getFileHandler(String path) {
+        if(!fileHandlers.containsValue(path)){
+            return fileHandlers.get(path);
+        }
+        else {
+            Thread t = new Thread();
+            t.start();
+            return t;
+        }
     }
 
 }
